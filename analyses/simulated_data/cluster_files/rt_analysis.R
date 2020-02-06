ac0236b6f45735797e93790724cdcadd0168a88b
PhilBoileau <philippe_boileau@berkeley.edu>
2020-02-06 08:36:43 -0800

    ironed out typos in sim data rt analysis

--- analyses/simulated_data/cluster_files/rt_analysis.R
+++ analyses/simulated_data/cluster_files/rt_analysis.R
@@ -50,14 +50,12 @@ sim_microbench <- microbenchmark(
   "cPCA" = scPCA(tg, bg, center = TRUE, scale = TRUE,
                  penalties = 0, n_centers = 2, parallel = TRUE),
   "scPCA (iterative)" = scPCA(tg, bg, center = TRUE, scale = TRUE,
-                              n_centers = 2, parallel = TRUE, alg = "iterative",
-                              parallel = TRUE),
+                              n_centers = 2, parallel = TRUE, alg = "iterative"),
   "scPCA (var. proj.)" = scPCA(tg, bg, center = TRUE, scale = TRUE,
-                               n_centers = 2, parallel = TRUE, alg = "var_proj",
-                               parallel = TRUE),
+                               n_centers = 2, parallel = TRUE, alg = "var_proj"),
   "scPCA (rand. var. proj.)" = scPCA(tg, bg, center = TRUE, scale = TRUE,
                                      n_centers = 2, parallel = TRUE,
-                                     alg = "rand_var_proj", parallel = TRUE),
+                                     alg = "rand_var_proj"),
   "t-SNE (PCA)" = Rtsne(scale(tg), theta = 0, perplexity = 30, max_iter = 1000,
                         pca = TRUE, num_threads = 4),
   "t-SNE" = Rtsne(scale(tg), theta = 0, perplexity = 30, max_iter = 1000,
@@ -67,17 +65,17 @@ sim_microbench <- microbenchmark(
     sim_groups_sce[, which(sim_groups_sce$Group != "Group2")],
     K = 2
   ),
-  "ZINB-WaVE (no batch)" = zinbwave(
+  "ZINB-WaVE (batch)" = zinbwave(
     sim_groups_sce[, which(sim_groups_sce$Group != "Group2")],
     K = 2,
     X = "~Batch"
   ),
   "SIMLR" = SIMLR(X = t(scale(tg)), c = 2, no.dim = 2, normalize = FALSE,
                   cores.ratio = 4),
-  times = 10,
+  times = 5,
   control = list(order = "block")
 )
 
 # save the file for later
-write_rds(sim_scpca_cv,
+write_rds(sim_microbench,
           path = here("analyses/simulated_data/cluster_files/sim_microbench.rds"))
